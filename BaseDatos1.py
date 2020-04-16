eb7aae3225ea952bf89a8165bd42b4e38269123f
root <root@localhost.localdomain>
2020-04-16 11:22:14 -0400

    16042020-03

--- BaseDatos1.py
+++ BaseDatos1.py
@@ -5,6 +5,8 @@ miConexion=sqlite3.connect("PrimeraBase")
 #crear tabla
 miCursor=miConexion.cursor()
 
-miCursor.execute("CREATE TABLE PRODUCTOS(NOMBRE_ARTICULO VARCHAR(50),PRECIO INTEGER,SECCION VARCHAR(20))")
+#miCursor.execute("CREATE TABLE PRODUCTOS(NOMBRE_ARTICULO VARCHAR(50),PRECIO INTEGER,SECCION VARCHAR(20))")
+
+miCursor.execute("INSERT INTO PRODUCTOS VALUES ('BALON',12,'DEPORTES')")
 
 miConexion.close()
