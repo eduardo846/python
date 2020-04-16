cc2e50e68bbc44d3454036a1def7edbb57c672c9
root <root@localhost.localdomain>
2020-04-16 11:27:44 -0400

    16042020-03

--- BaseDatos1.py
+++ BaseDatos1.py
@@ -8,5 +8,6 @@ miCursor=miConexion.cursor()
 #miCursor.execute("CREATE TABLE PRODUCTOS(NOMBRE_ARTICULO VARCHAR(50),PRECIO INTEGER,SECCION VARCHAR(20))")
 
 miCursor.execute("INSERT INTO PRODUCTOS VALUES ('BALON',12,'DEPORTES')")
+miConexion.commit()
 
 miConexion.close()
